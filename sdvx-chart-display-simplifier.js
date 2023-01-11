c46a200b738ae35575ebecd7245b8e8eebc0080d
70165tk <65953662+70165tk@users.noreply.github.com>
2023-01-12 01:10:26 +0900

    実装

--- sdvx-chart-display-simplifier.js
+++ sdvx-chart-display-simplifier.js
@@ -1,1 +1,27 @@
 // TODO: つまみの自動簡略化スクリプト
+{
+    const s = document.querySelector(".chartSimplifier")
+
+    s.addEventListener("click", function () {
+        const simplified = "simplified" in s.dataset
+        if (simplified) {
+            delete s.dataset.simplified
+        } else {
+            s.dataset.simplified = "simplified"
+        }
+        reflectToChartChanger(simplified)
+    })
+
+    function reflectToChartChanger(simplified) {
+        const chartChanger = document.querySelector(`.chartChanger`)
+        if (chartChanger) {
+            if (simplified) {
+                chartChanger.dataset.chartReplacePattern = "STRAIGHT_"
+                chartChanger.dataset.chartReplacement = ""
+            } else {
+                chartChanger.dataset.chartReplacePattern = "CURVE"
+                chartChanger.dataset.chartReplacement = "STRAIGHT_$&"
+            }
+        }
+    }
+}
